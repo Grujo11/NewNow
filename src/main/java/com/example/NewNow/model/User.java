53bc075e9b912b9005dbdd5b43bc090d20125f1f
Grujo11 <stefangrujicic00@gmail.com>
2025-11-01 15:20:54 +0100

    povezivanje sa bazom

--- src/main/java/com/example/NewNow/model/User.java
+++ src/main/java/com/example/NewNow/model/User.java
@@ -1,8 +1,20 @@
 package com.example.NewNow.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Table (name = "user")
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String password;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users(email)VALUES
+('stefan@gmail.com',)
